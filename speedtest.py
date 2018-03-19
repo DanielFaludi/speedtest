ae841c57f2c24574dfab490a034b0b1859cb52ce
Danosaurus <danielfaludi2@gmail.com>
2018-03-19 18:47:12 +0100

    Finished function for choosing server

--- speedtest.py
+++ speedtest.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python3
 
+import os
 import re
 import logging
 import requests
-import subprocess
+import bisect
+
+from math import sqrt
+from itertools import product
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 logging.debug('Start of program...')
+
+def server_is_up(server):
+    hostname = server
+    response = os.system('ping -c 1 ' + hostname)
+    if response == 0:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        return True
+    else:
+        return False
 
 def choose_server():
     get = requests.get('http://www.speedtest.net/speedtest-config.php?')
@@ -17,20 +30,31 @@ def choose_server():
     if coordinates is None:
         logging.debug('Failed to obtain location')
     location = coordinates.groups()
-    user_lat = float(location[1])
+    user_lat = float(location[1]) 
     user_lon = float(location[2])
     get = requests.get('http://www.speedtest.net/speedtest-servers.php?')
     if not get.status_code == 200:
         logging.debug('GET Request failed')
     reply = get.text
-    server_list = re.findall(r'<server url="([^"]*)" lat="([^"]*)" lon="([^"]*)"', reply)
+    server_list = re.findall(r'<server url="http://([^/]*)/speedtest/upload\.php" lat="([^"]*)" lon="([^"]*)"', reply)
     if server_list is None:
         logging.debug('Failed to obtain server list')
     server_list = [list(server) for server in server_list]
-    s_lat_list = [float(server[1]) for server in server_list]
-    s_lon_list = [float(server[2]) for server in server_list]
-    closest_lat = min(s_lat_list, key=lambda x: abs(x - user_lat))
-    closest_lon = min(s_lon_list, key=lambda x: abs(x - user_lon))
+    server_adrr_list = []
+    for server in server_list:
+        server_lat = float(server[1])
+        server_lon = float(server[2])
+        distance = sqrt(pow(server_lat - user_lat, 2) + pow(server_lon - user_lon, 2))
+        bisect.insort_left(server_adrr_list, (distance, server[0]))
+    chosen_server = ''
+    for server in server_adrr_list[:10]:
+        if server_is_up(server[1]):
+            chosen_server = server[1]
+            return chosen_server
+        else:
+            continue
+    if chosen_server == '':
+        logging.debug("Could not choose a server")
 
 if __name__ == '__main__':
     choose_server()
